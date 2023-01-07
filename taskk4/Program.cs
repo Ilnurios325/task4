cda9ba57133c5af947701b37c825a76aaa22f14b
Ilnurios325 <ipilulka@gmail.com>
2023-01-07 10:33:46 +0300

    solution

---
+++ taskk4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("enter num1: ");
+int num1 = 
+Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter num2: ");
+int num2 = 
+Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter num3: ");
+int num3 = 
+Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+{
+    Console.WriteLine("maximum number = "+max);
+}    
